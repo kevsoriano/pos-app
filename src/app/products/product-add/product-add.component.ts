0142b29a955b14ec268d091774fcd7e475fba00e
kevsoriano <kevinsoriano173@gmail.com>
2023-12-02 18:01:11 +0800

    fix: add product variant undefined issue

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -9,8 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-add.component.scss']
 })
 export class ProductAddComponent {
-
   items = ['angular', 'chip', 'tutorial'];
+  attributesArray: Array<{attributeType: string, attributeValues: Array<string>}> = [];
+
   form = new FormGroup({
     name: new FormControl(''),
     productVariants: new FormArray([])
@@ -31,10 +32,16 @@ export class ProductAddComponent {
   }
 
   addProductAttributes() {
-    const productAttribute = new FormGroup({
-      attributeType: new FormControl(''),
-      attributeValue: new FormControl('')
-    });
+    // const attribute = new FormGroup({
+    //   attributeType: new FormControl(''),
+    //   attributeValue: new FormControl('')
+    // });
     // this.productVariants.push(productVariant);
+    const attribute = {
+      attributeType: "",
+      attributeValues: [""]
+    };
+    this.attributesArray.push(attribute);
+    console.log(this.attributesArray);
   }
 }
