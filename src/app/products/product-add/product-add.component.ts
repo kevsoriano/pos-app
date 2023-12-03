d70ba9d92e725c2bc7bdfce8e4a217c0aaf7d4ec
Kevin Gil Soriano <kevingilsoriano@Kevins-Mac-mini.local>
2023-12-03 18:10:49 +0800

    update

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
@@ -9,18 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-add.component.scss']
 })
 export class ProductAddComponent {
-  items = ['angular', 'chip', 'tutorial'];
   attributesArray: Array<{attributeType: string, attributeValues: Array<string>}> = [];
-
+  placeholder: string = 'Enter an attribute value';
+  removable = true;
   form = new FormGroup({
     name: new FormControl(''),
     productVariants: new FormArray([])
   });
+  @ViewChild('attributeValueField') attributeValueField: any;
 
   constructor(private productService: ProductService, private router: Router) {}
 
   get productVariants() {
     return (this.form.get('productVariants') as FormArray).controls;
+  }
+
+  getAttributeValues(index: number) {
+    return this.attributesArray[index].attributeValues;
   }
 
   onSubmit() {
@@ -37,30 +42,52 @@ export class ProductAddComponent {
       attributeValues: []
     };
     this.attributesArray.push(attribute);
-    console.log(this.attributesArray);
   }
 
-  onKeyDown($event: KeyboardEvent, index: number, attributeType: string, attributeValue: string) {
-    console.log(attributeValue);
+  onAttributeTypeKeyDown($event: KeyboardEvent, index: number, attributeType: string) {
+    switch($event.keyCode) {
+      case 13:
+        if(attributeType && attributeType.trim() !== '') {
+          this.attributesArray[index].attributeType = attributeType;
+          console.log(this.attributesArray)
+          $event.preventDefault();
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  onAttributeValueKeyDown($event: KeyboardEvent, index: number, attributeValue: string) {
     switch($event.keyCode) {
       case 13:
         if(attributeValue && attributeValue.trim() !== '') {
           if(!this.attributesArray[index].attributeValues.includes(attributeValue)) {
             this.attributesArray[index].attributeValues.push(attributeValue);
+            this.createProductVariants();
           }
-          // this.inputField.nativeElement.value = '';
-          console.log(this.attributesArray)
+          this.attributeValueField.nativeElement.value = '';
           $event.preventDefault();
         }
         break;
-      // case 8: {
-      //   if(!value && this.items.length > 0) {
-      //     this.attributesArray.pop();
-      //   }
-      //   break;
-      // }
+      case 8: {
+        if(!attributeValue && this.attributesArray[index].attributeValues.length > 0) {
+          this.attributesArray[index].attributeValues.pop();
+        }
+        break;
+      }
       default:
         break;
     }
+  }
+
+  createProductVariants() {
+    this.attributesArray.forEach((attribute)=> {
+      console.log(attribute.attributeType);
+    });
+  }
+
+  removeItem(index: number, position: number) {
+    this.attributesArray[index].attributeValues.splice(position, 1);
   }
 }
